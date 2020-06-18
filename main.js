b050f16b6e568dc18cf9813e6d880e6b69be3a76
Tikhaya Anastasiia <tikhaya.a.v@gmail.com>
2020-06-18 15:25:37 +0300

    form_js

--- main.js
+++ main.js
@@ -16,20 +16,22 @@ $(document).ready(function(){
     let form = document.getElementsByClassName("block-form");
     let text = document.getElementsByClassName("block-text");
     let element = document.getElementsByClassName("form_open");
-    
     function openForm(){
         form[0].style.display = 'block';
         text[0].style.display = 'none';
     }
-    element[0].addEventListener("click", openForm);
+    if(element.length > 0){
+        element[0].addEventListener("click", openForm);
+    }    
         
     let returnLink = document.getElementsByClassName("returnLink");
     function closeForm(){
         form[0].style.display = 'none';
         text[0].style.display = 'block';
     }
+    if(element.length > 0){
     returnLink[0].addEventListener("click", closeForm);
-
+    }
 
     let openLog = document.getElementsByClassName("openLogin");
     let formLog = document.getElementsByClassName("form-log-wrap");
@@ -39,5 +41,29 @@ $(document).ready(function(){
         blackout[0].style.display = 'block';
     }
     openLog[0].addEventListener("click", openFormLog);
+
+    let mobileNav = document.getElementsByClassName("mobile-nav");
+    let desctopNav = document.getElementsByClassName("desctop-nav");
+        function openNav(){
+            desctopNav[0].style.display = 'block';
+            blackout[0].style.display = 'block';
+        }
+    mobileNav[0].addEventListener("click", openNav);
+
+    let likeBtm = document.getElementsByClassName("user-review_block_like");
+    function likeAdd(element){
+        let total = element.lastElementChild.innerHTML;
+        let newTotal = Number(total) + 1;
+        element.lastElementChild.innerHTML = newTotal;
+    }
+    let newLikeBtn = Object.values(likeBtm);
+
+    newLikeBtn.forEach(element => {
+       element.addEventListener("click", () => likeAdd(element));
+    });
+    
+    
+
+    
 
 });
